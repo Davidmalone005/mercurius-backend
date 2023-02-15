d9d55ae4829acb45e891176a6fc68304825aea41
Onimisea <onimisea@gmail.com>
2023-02-15 08:05:55 +0100

    Updated url params

--- users/urls.py
+++ users/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path("register/", views.CreateUserAPIView.as_view(), name="register"),
     path("login/", views.LoginUserAPIView.as_view(), name="login"),
     path("verify/", views.VerifyUserAPIView.as_view()),
-    path("update/<str:pk>/", views.UpdateUserAPIView),
-    path("<str:pk>/change-password/", views.ChangePasswordAPIView),
-    path("<str:id>/", views.UserDetailAPIView.as_view()),
+    path("update/<int:pk>/", views.UpdateUserAPIView),
+    path("<int:pk>/change-password/", views.ChangePasswordAPIView),
+    path("<int:id>/", views.UserDetailAPIView.as_view()),
     # path("delete/<str:id>/", views.DeleteUserAPIView.as_view()),
 ]
